4ab70cb5a11889e3e7aa334b41c40ce989f634a3
nemonowa <105987977+nemonowa@users.noreply.github.com>
2025-02-13 15:32:41 +0900

    new

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// メニューの表示・非表示を切り替える関数
 function toggleMenu(menuId) {
     const menus = document.querySelectorAll('.menu');
     menus.forEach(menu => {
@@ -8,3 +9,19 @@ function toggleMenu(menuId) {
         }
     });
 }
+
+// header.html を読み込む
+fetch('https://nemonowa.github.io/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading header:', error));
+
+// footer.html を読み込む
+fetch('https://nemonowa.github.io/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading footer:', error));
